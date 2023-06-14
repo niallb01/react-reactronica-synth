c941888ef44fabbb3e26770648d5c2330d2afce1
Niall Bennett <niallbennett@Nialls-MacBook-Pro.local>
2023-06-14 21:39:55 +0100

    dial knob added

--- src/components/Keyboard.jsx
+++ src/components/Keyboard.jsx
@@ -1,6 +1,28 @@
+import { useState } from "react";
+import { Silver } from "react-dial-knob";
+
 const Keyboard = (props) => {
+  const [reverbValue, setReverbValue] = useState(0);
+
   return (
     <>
+      <Silver
+        // className="dial-knob"
+        diameter={200}
+        min={0}
+        max={100}
+        step={1}
+        value={reverbValue}
+        theme={{
+          donutColor: "blue",
+          //   width: "30px",
+        }}
+        onValueChange={setReverbValue}
+        ariaLabelledBy={"my-label"}
+      >
+        <label id={"my-label"}>Reverb</label>
+      </Silver>
+
       <div className="keyboard-container">
         <ul className="synth-keys">
           <li className="white-key-li"></li>
